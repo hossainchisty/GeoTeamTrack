5d55cae0cfcd9005b14072704a2968b74c67f0a4
hossainchisty <hossain.chisty11@gmail.com>
2023-10-30 18:18:10 +0600

    feat: Added helper functions

---
+++ src/helpers/jwtHelper.ts
@@ -0,0 +1,21 @@
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
+import { JWTPayload } from "../interface/common";
+
+const generateToken = (
+  payload: JWTPayload,
+  secret: Secret,
+  expireTime: string
+) => {
+  const token = jwt.sign(payload, secret, { expiresIn: expireTime });
+  return token;
+};
+
+const verifyToken = (token: string, secret: Secret): JwtPayload => {
+  return jwt.verify(token, secret, { algorithms: ['HS256'] }) as JwtPayload;
+};
+
+
+export const jwtHelper = {
+  generateToken,
+  verifyToken,
+};
